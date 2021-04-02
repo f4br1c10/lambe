6292efc8ad39e7c59f27fdf1391b27c6940aab2f
f4br1c10 <fabriciopstos@hotmail.com>
2021-04-02 14:40:30 -0300

    Tela de adicionar foto

--- src/Navigator.js
+++ src/Navigator.js
@@ -4,6 +4,7 @@
     import Icon from 'react-native-vector-icons/FontAwesome'
     
     import Feed from './screens/Feed'
+    import AddPhoro from './screens/AddPhoto'
 
     const Tab = createBottomTabNavigator()
     
@@ -32,7 +33,7 @@
                         })}
                         tabBarOptions={{ showLabel: false }}>
                         <Tab.Screen name='Feed' component={Feed} />
-                        <Tab.Screen name='AddPhoto' component={Feed} />
+                        <Tab.Screen name='AddPhoto' component={AddPhoro} />
                         <Tab.Screen name='Profile' component={Feed} />
                     </Tab.Navigator>
                 </NavigationContainer>
