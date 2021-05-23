4654439ada9556fe5a473f7c6f2efefd8c374bb5
f4br1c10 <fabriciopstos@hotmail.com>
2021-05-23 18:30:45 -0300

    Usando Firebase Storage #2

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require("firebase-functions");
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+    projectId: 'lambe-a423c',
+    keyFilename: 'lambe-a423c.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+exports.uploadImage = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-
+    })
 });
